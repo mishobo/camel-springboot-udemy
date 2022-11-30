ae90b81a7c602201790b99722f1d05028411e0fa
mishobo <60912349+mishobo@users.noreply.github.com>
2022-11-30 10:27:57 +0300

    expectedBody testing

--- src/test/java/com/husseinabdallah/camelspringbootudemy/LegacyFileRouteTest.java
+++ src/test/java/com/husseinabdallah/camelspringbootudemy/LegacyFileRouteTest.java
@@ -2,6 +2,7 @@ package com.husseinabdallah.camelspringbootudemy;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.EndpointInject;
+import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.AdviceWith;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
@@ -21,6 +22,9 @@ public class LegacyFileRouteTest {
     @EndpointInject("mock:result")
     protected MockEndpoint mockEndpoint;
 
+    @Autowired
+    ProducerTemplate producerTemplate;
+
     @Test
     public void testFileMove() throws Exception{
         //Setup the mock
@@ -36,6 +40,23 @@ public class LegacyFileRouteTest {
         //Start the context and validate is mock is asserted
         context.start();
         mockEndpoint.assertIsSatisfied();
-
     }
+
+    @Test
+    public void testFileMoveByMockingFromEndpoint() throws Exception {
+        String expectedBody = "This is input data after mocking the from endpoint";
+        mockEndpoint.expectedBodiesReceived(expectedBody);
+        mockEndpoint.expectedMinimumMessageCount(1);
+
+        AdviceWith.adviceWith(context,"legacyFileMoveRouteId", routeBuilder ->{
+            routeBuilder.replaceFromWith("direct:mockStart");
+            routeBuilder.weaveByToUri("file:*").replace().to(mockEndpoint);
+        });
+
+        context.start();
+        producerTemplate.sendBody("direct:mockStart", expectedBody);
+        mockEndpoint.assertIsSatisfied();
+    }
+
+
 }
