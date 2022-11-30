25df5bd423932e61463d4fd19cd5c40cafe2d532
mishobo <60912349+mishobo@users.noreply.github.com>
2022-11-30 08:16:00 +0300

    rename application name

--- src/main/java/com/husseinabdallah/camelspringbootudemy/CamelSpringBootUdemyApplication.java
+++ src/main/java/com/husseinabdallah/camelspringbootudemy/CamelSpringBootUdemyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CamelSpringbootUdemyApplication {
+public class CamelSpringBootUdemyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CamelSpringbootUdemyApplication.class, args);
+		SpringApplication.run(CamelSpringBootUdemyApplication.class, args);
 	}
 
 }
